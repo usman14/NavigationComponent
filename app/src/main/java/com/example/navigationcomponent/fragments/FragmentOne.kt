3ba1766f68569ea23ac6ef4ff6858d62df593b71
Usman Zafer <usman.zafer@open-xchange.com>
2020-04-04 08:07:53 +0200

    Revert "Setting custom toolbar in Fragment"

--- app/src/main/java/com/example/navigationcomponent/fragments/FragmentOne.kt
+++ app/src/main/java/com/example/navigationcomponent/fragments/FragmentOne.kt
@@ -2,51 +2,21 @@ package com.example.navigationcomponent.fragments
 
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import com.example.navigationcomponent.R
 
+import com.example.navigationcomponent.R
 
 class FragmentOne : Fragment() {
 
-    private lateinit var toolbar: Toolbar
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
-        toolbar = view.findViewById(R.id.toolbar)
-        toolbar.inflateMenu(R.menu.toolbar_items)
-        toolbar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.action_item_one -> {
-                    // do something
-                    true
-                }
-                R.id.action_item_two -> {
-                    // do something
-                    true
-                }
-                else -> {
-                    super.onOptionsItemSelected(it)
-                }
-            }
-        }
-        
-        toolbar.setNavigationOnClickListener {
-            // do something when click navigation
-        }
-
     }
 
 
